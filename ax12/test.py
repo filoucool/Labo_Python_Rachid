90bbc0931a3c75deb9caffab1fd502da75b1308e
Félix Chenette-Stewart <felix.chenette@gmail.com>
2021-03-29 16:53:48 -0400

    Create test.py

---
+++ ax12/test.py
@@ -0,0 +1,14 @@
+import time
+import ax12
+import RPi.GPIO as GPIO
+import serial as Serial
+
+
+def main():
+    moveSpeed(18, 10, 500):
+    moveSpeed(17, 90, 500):
+    moveSpeed(18, 120, 500):
+    
+    
+if __name__ == '__main__':
+    main()
