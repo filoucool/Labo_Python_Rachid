d383324e74f13fb781393cd23602169f635ec569
Félix Chenette-Stewart <felix.chenette@gmail.com>
2021-04-09 15:10:01 -0400

    Update Lab_01_b.py

--- ax12/Lab_01_b.py
+++ ax12/Lab_01_b.py
@@ -5,30 +5,30 @@ import time
 
 
 delay_0=0.008
-ax12_o = Ax12()
-ax12_o.__init__()
+ax12_Lib = Ax12()
+ax12_Lib.__init__()
 time.sleep(2)
 
 
 dynamixel_id1 = 17
 dynamixel_id2 = 18
-ax12_o.ping(dynamixel_id1)
+ax12_Lib.ping(dynamixel_id1)
 time.sleep(delay_0)
-ax12_o.ping(dynamixel_id2)
+ax12_Lib.ping(dynamixel_id2)
 time.sleep(delay_0)
 
 
-ax12_o.setAngleLimit(dynamixel_id1, 0, 1023)
+ax12_Lib.setAngleLimit(dynamixel_id1, 0, 1023)
 time.sleep(delay_0)
-ax12_o.setAngleLimit(dynamixel_id2, 0, 1023)
+ax12_Lib.setAngleLimit(dynamixel_id2, 0, 1023)
 time.sleep(1)
 
-ax12_o.move(dynamixel_id1, 0)
+ax12_Lib.move(dynamixel_id1, 0)
 time.sleep(delay_0)
 time.sleep(0.1)
 time.sleep(3)
-ax12_o.move(dynamixel_id1, 1023)
+ax12_Lib.move(dynamixel_id1, 1023)
 time.sleep(3)
 
 
-ax12_o.direction(Ax12.RPI_DIRECTION_TX)
+ax12_Lib.direction(Ax12.RPI_DIRECTION_TX)
