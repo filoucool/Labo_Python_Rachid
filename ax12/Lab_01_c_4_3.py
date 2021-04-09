ab59e2354d467aee6fe9fdc8773ec612c182a4a9
Félix Chenette-Stewart <felix.chenette@gmail.com>
2021-04-09 15:11:32 -0400

    Update Lab_01_c_4_3.py

--- ax12/Lab_01_c_4_3.py
+++ ax12/Lab_01_c_4_3.py
@@ -5,52 +5,52 @@ import time
 
 
 delay_0=0.001
-ax12_o = Ax12()
-ax12_o.__init__()
+ax12_Lib = Ax12()
+ax12_Lib.__init__()
 time.sleep(1)
 
 
 dynamixel_id1 = 17
 dynamixel_id2 = 18
-ax12_o.ping(dynamixel_id1)
+ax12_Lib.ping(dynamixel_id1)
 time.sleep(delay_0)
-ax12_o.ping(dynamixel_id2)
+ax12_Lib.ping(dynamixel_id2)
 time.sleep(delay_0)
 
 
-ax12_o.setAngleLimit(dynamixel_id1, 0, 0)
+ax12_Lib.setAngleLimit(dynamixel_id1, 0, 0)
 time.sleep(delay_0)
-ax12_o.setAngleLimit(dynamixel_id2, 0, 0)
+ax12_Lib.setAngleLimit(dynamixel_id2, 0, 0)
 time.sleep(1)
 
 time.sleep(1)
-ax12_o.Speed(dynamixel_id1, 1023+250)
+ax12_Lib.Speed(dynamixel_id1, 1023+250)
 time.sleep(delay_0)
 
 i=0
-pos=ax12_o.readPosition(dynamixel_id1)
+pos=ax12_Lib.readPosition(dynamixel_id1)
 time.sleep(delay_0)
 print(pos)
 while pos>5:
     i = i+1
-    pos=ax12_o.readPosition(dynamixel_id1)
+    pos=ax12_Lib.readPosition(dynamixel_id1)
     print(pos)
     if (pos<30):
-        ax12_o.Speed(dynamixel_id1, 1023+(pos*5))
+        ax12_Lib.Speed(dynamixel_id1, 1023+(pos*5))
         time.sleep(delay_0) 
     time.sleep(0.1) 
 
-pos=ax12_o.readPosition(dynamixel_id1)
+pos=ax12_Lib.readPosition(dynamixel_id1)
 time.sleep(delay_0)
 print(pos)
 
 time.sleep(1)
-ax12_o.Speed(dynamixel_id1, 0)
+ax12_Lib.Speed(dynamixel_id1, 0)
 time.sleep(delay_0)
-ax12_o.Speed(dynamixel_id2, 0)
+ax12_Lib.Speed(dynamixel_id2, 0)
 time.sleep(delay_0)
 
-ax12_o.direction(Ax12.RPI_DIRECTION_TX)
+ax12_Lib.direction(Ax12.RPI_DIRECTION_TX)
 
 
 
